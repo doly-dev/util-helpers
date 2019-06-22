a5ee14a928e9df8c7e8bb31d8a199a36d256e50e
蔡金锋 <jeff.cai@99bill.com>
2019-06-23 00:37:28 +0800

    更新文档

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,15 +1,33 @@
+import isEmail from './isEmail'
+import isIdCard from './isIdCard'
 import isMobile from './isMobile'
+import isPostcard from './isPostcode'
 import isQQ from './isQQ'
+import isTelephone from './isTelephone'
+import isVehicle from './isVehicle'
+import isWX from './isWX'
 
 /**
  * 数据验证
  *
  * @module validator
  * @since 1.1.0
+ * @requires validator/isEmail
+ * @requires validator/isIdCard
  * @requires validator/isMobile
+ * @requires validator/isPostcard
  * @requires validator/isQQ
+ * @requires validator/isTelephone
+ * @requires validator/isVehicle
+ * @requires validator/isWX
 */
 export default {
+    isEmail,
+    isIdCard,
     isMobile,
-    isQQ
+    isPostcard,
+    isQQ,
+    isTelephone,
+    isVehicle,
+    isWX
 };
