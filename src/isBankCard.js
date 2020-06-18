81c8f0797a73e5ed245709c24bc47225d1ee0656
蔡金锋 <jeff.cai@99bill.com>
2020-06-18 10:58:20 +0800

    校验银行卡号支持宽松模式

--- src/isBankCard.js
+++ src/isBankCard.js
@@ -1,13 +1,19 @@
-// 银行卡号正则 非0开头，15至19位数字
+// 非0开头，15~19位数字
 const reg = /^[1-9]\d{14,18}$/;
 
+// 8~30位数字
+const regLoose = /^\d{8,30}$/;
+
 /**
- * 检测值是否为银行卡号
+ * 检测值是否为银行卡号。正常模式（非0开头，15~19位数字）宽松模式（8~30位数字）
  * 
  * @static
  * @alias module:Validator.isBankCard
+ * @see {@link https://kf.qq.com/faq/170112ABnm6b170112FvquAn.html|常用银行账号位数参考}
  * @since 1.1.0
  * @param {String} value 要检测的值
+ * @param {Object} [options] 配置项
+ * @param {Boolean} [options.loose=false] 宽松模式，默认 false
  * @returns {Boolean} 值是否为银行卡号
  * @example
  * 
@@ -20,8 +26,16 @@ const reg = /^[1-9]\d{14,18}$/;
  * isBankCard('123456789');
  * // => false
  * 
+ * isBankCard('123456789', { loose: true });
+ * // => true
+ * 
  */
-function isBankCard(value) {
+function isBankCard(value, {
+  loose = false
+} = {}) {
+  if (loose) {
+    return regLoose.test(value);
+  }
   return reg.test(value);
 }
 
--- test/validator/isBankCard.test.js
+++ test/validator/isBankCard.test.js
@@ -18,4 +18,13 @@ describe('isBankCard', () => {
   it('"123456789" => false', () => {
     expect(isBankCard('123456789')).to.be.equal(false);
   });
+  it('宽松模式 "123456789" => true', () => {
+    expect(isBankCard('123456789', { loose: true })).to.be.equal(true);
+  });
+  it('宽松模式 "123456789012345678901234567890" => true', () => {
+    expect(isBankCard('123456789012345678901234567890', { loose: true })).to.be.equal(true);
+  });
+  it('宽松模式 "1234567890123456789012345678901" => false', () => {
+    expect(isBankCard('1234567890123456789012345678901', { loose: true })).to.be.equal(false);
+  });
 })
