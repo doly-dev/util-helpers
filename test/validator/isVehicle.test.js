ded4d4433990787dc22d45b41f72c1b5597e1cb0
蔡金锋 <jeff.cai@99bill.com>
2020-07-31 15:02:36 +0800

    车牌号校验支持新能源车牌

--- test/validator/isVehicle.test.js
+++ test/validator/isVehicle.test.js
@@ -10,10 +10,28 @@ describe('isVehicle', () => {
     expect(isVehicle(true)).to.be.equal(false);
     expect(isVehicle(123)).to.be.equal(false);
   });
-  it('"京L88888" => true', () => {
-    expect(isVehicle('京L88888')).to.be.equal(true);
+  it('"BL12345" => true', () => {
+    expect(isVehicle('京L12345')).to.be.equal(true);
   });
-  it('"333333" => false', () => {
-    expect(isVehicle('333333')).to.be.equal(false);
+  it('"BL1234警" => true', () => {
+    expect(isVehicle('京L12345')).to.be.equal(true);
+  });
+  it('"京L1234领" => true', () => {
+    expect(isVehicle('京L12345')).to.be.equal(true);
+  });
+  it('"京L12345" => true', () => {
+    expect(isVehicle('京L12345')).to.be.equal(true);
+  });
+  it('"粤BD12345" => true', () => {
+    expect(isVehicle('粤BD12345')).to.be.equal(true);
+  });
+  it('"粤BF12345" => true', () => {
+    expect(isVehicle('粤BF12345')).to.be.equal(true);
+  });
+  it('"粤B12345D" => true', () => {
+    expect(isVehicle('粤B12345D')).to.be.equal(true);
+  });
+  it('"粤B12345F" => true', () => {
+    expect(isVehicle('粤B12345F')).to.be.equal(true);
   });
 })
