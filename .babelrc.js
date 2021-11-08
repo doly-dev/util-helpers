3d836412cc22e18960fda8c1dc0867707ad6526a
蔡金锋 <jeff.cai@99bill.com>
2021-11-08 17:23:08 +0800

    chore: cjs去掉@babel/runtime

--- .babelrc.js
+++ .babelrc.js
@@ -2,28 +2,23 @@ const { MODULE_TYPE } = process.env;
 
 const plugins = [];
 
-if (MODULE_TYPE === "cjs") {
-  plugins.push("@babel/transform-modules-commonjs");
+if (MODULE_TYPE === 'cjs') {
+  plugins.push('@babel/transform-modules-commonjs');
 }
 
-if (MODULE_TYPE !== "umd") {
-  plugins.push("@babel/transform-runtime");
+if (MODULE_TYPE === 'esm') {
+  plugins.push('@babel/transform-runtime');
 }
 
 module.exports = {
-  "presets": [
+  presets: [
     [
-      "@babel/env",
+      '@babel/env',
       {
-        "modules": MODULE_TYPE === "esm" ? false : "auto",
-        "targets": [
-          "> 1%",
-          "last 4 versions",
-          "Firefox ESR",
-          "not ie < 9"
-        ]
+        modules: MODULE_TYPE === 'esm' ? false : 'auto',
+        targets: ['> 1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
       }
     ]
   ],
   plugins
-}
+};
