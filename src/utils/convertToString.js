b50b03cdb4d589b37b716a6a5e98c6b38a99e3ea
蔡金锋 <jeff.cai@99bill.com>
2021-11-19 15:05:00 +0800

    chore: 去掉多余注释

--- src/utils/convertToString.js
+++ src/utils/convertToString.js
@@ -1,5 +1,4 @@
 import isString from './type/isString';
-import { config } from './config';
 
 /**
  * 转换为字符串
@@ -8,13 +7,7 @@ import { config } from './config';
  * @returns 字符串
  */
 function convertToString(value) {
-  if (!isString(value)) {
-    if (!config.disableWarning) {
-      console.warn(`[validatePassword] value must be a string.`);
-    }
-    return String(value);
-  }
-  return value;
+  return isString(value) ? value : String(value);
 }
 
 export default convertToString;
