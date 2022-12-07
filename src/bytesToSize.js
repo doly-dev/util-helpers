e1da679c0d41e26ce5c5619d8a8464c632f15683
caijf <369756941@qq.com>
2022-12-07 19:42:27 +0800

    feat: bytesToSize 方法支持自定义间隔字符

--- src/bytesToSize.js
+++ src/bytesToSize.js
@@ -5,6 +5,8 @@
  * @alias module:Processor.bytesToSize
  * @since 3.8.0
  * @param {number} bytes 字节大小
+ * @param {Object} [options] 配置项
+ * @param {string} [options.spaceMark=' '] 间隔字符
  * @returns {string} 存储单位值
  * @example
  *
@@ -13,17 +15,23 @@
  * bytesToSize(3.213243*1024*1024); // 3.21 MB
  * bytesToSize(1024*1024*1024); // 1 GB
  * 
+ * // 自定义间隔符号
+ * bytesToSize(0, { spaceMark: '' }); // 0B
+ * bytesToSize(1024, { spaceMark: '' }); // 1KB
+ * 
  */
-function bytesToSize(bytes) {
+function bytesToSize(bytes, options = {}) {
+  const { spaceMark = ' ' } = options;
+
   const numBytes = typeof bytes !== 'number' ? Number(bytes) : bytes;
-  if (numBytes === 0 || isNaN(numBytes)) return '0 B';
+  if (numBytes === 0 || isNaN(numBytes)) return `0${spaceMark}B`;
 
   const k = 1024;
   // 存储单位
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(numBytes) / Math.log(k));
 
-  return sizes[i] ? `${Number((numBytes / Math.pow(k, i)).toFixed(2))} ${sizes[i]}` : numBytes + '';
+  return sizes[i] ? `${Number((numBytes / Math.pow(k, i)).toFixed(2))}${spaceMark}${sizes[i]}` : numBytes + '';
 }
 
 export default bytesToSize;
--- test/processor/bytesToSize.test.js
+++ test/processor/bytesToSize.test.js
@@ -29,4 +29,11 @@ describe('bytesToSize', () => {
     expect(bytesToSize(10 * 1024 * 1024 * 1024)).toBe('10 GB');
     expect(bytesToSize(1425000000909)).toBe('1.3 TB');
   });
+
+  it('space mark', () => {
+    expect(bytesToSize(0, { spaceMark: '' })).toBe('0B');
+    expect(bytesToSize(1024, { spaceMark: '' })).toBe('1KB');
+    expect(bytesToSize(1024, { spaceMark: ' - ' })).toBe('1 - KB');
+    expect(bytesToSize(1025, { spaceMark: '%' })).toBe('1%KB');
+  })
 });
