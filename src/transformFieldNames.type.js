fb7b3e13fb80bea0dd7f077acc383f126e314e17
caijf <369756941@qq.com>
2023-04-11 14:51:04 +0800

    refactor: transformFieldNames 类型调整

--- src/transformFieldNames.type.js
+++ src/transformFieldNames.type.js
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 // 交换字段名
 /**
  * @template {*} D
@@ -8,10 +6,10 @@
  */
 
 // 交换字段名，支持嵌套
-// 先排除子级字段名，再交换字段名，然后加上子级字段名，再替换一次。这里的 F 有类型问题，因为排除了子级字段名，暂时没有比较好的方案处理。
+// 先排除子级字段名，再交换字段名，然后加上子级字段名，再替换一次。
 /**
  * @template {*} D
- * @template {Record<string, keyof D>} F
+ * @template {Record<string, any>} F
  * @template {string} C
  * @typedef {(C extends keyof D ? ExchangeFieldNames<Omit<D, C> & Record<C, TransformFieldNames<D, F, C>>, F> : ExchangeFieldNames<D, F>)[]} TransformFieldNames
  */
