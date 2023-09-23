90bd8b59695d94f804a7de4e0d9bd18f0f982b9c
caijf <369756941@qq.com>
2023-09-24 01:59:15 +0800

    refactor: 兼容小程序初始化没有读取到URL对象报错问题

--- src/utils/native.ts
+++ src/utils/native.ts
@@ -1,4 +1,6 @@
+import { constant, noop } from 'ut2';
+
 export const objectKeys = Object.keys;
 
-export const createObjectURL = URL.createObjectURL;
-export const revokeObjectURL = URL.revokeObjectURL;
+export const createObjectURL = typeof URL !== 'undefined' ? URL.createObjectURL : constant('');
+export const revokeObjectURL = typeof URL !== 'undefined' ? URL.revokeObjectURL : noop;
