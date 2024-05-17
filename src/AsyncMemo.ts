bb4a6436cd893a07b7881263a8aa585418182231
caijf <369756941@qq.com>
2024-05-17 09:48:39 +0800

    docs: update

--- src/AsyncMemo.ts
+++ src/AsyncMemo.ts
@@ -4,11 +4,25 @@ import { isString, uniqueId } from 'ut2';
 /**
  * 异步缓存
  *
+ * 特点：
+ * 1. 共享异步。同一个缓存键的异步，最多同时运行一个异步，异步结果共享。
+ * 2. 持久化数据。存在缓存结果，不再执行异步方法，直接返回该结果。
+ * 3. 每个实例都有独立的缓存空间。相互之间隔离，缓存灵活配置，更多配置请查阅 [`cache2`](https://www.npmjs.com/package/cache2)。
+ *
  * @class
  * @see {@link https://www.npmjs.com/package/cache2 | cache2}
  * @param {Object} [options] 缓存配置项，更多配置项可参考 [`cache2`](https://www.npmjs.com/package/cache2)
  * @param {number} [options.max] 最大缓存数量
  * @param {'replaced' | 'limited'} [options.maxStrategy] 缓存策略
+ * @example
+ *
+ * const asyncMemo = new AsyncMemo({ max: 20, maxStrategy: 'replaced' });
+ * asyncMemo(()=>download({ fssid: 'a' }), 'a');
+ * asyncMemo(()=>download({ fssid: 'b' }), 'b');
+ * asyncMemo(()=>download({ fssid: 'a' }), 'a'); // 如果有缓存结果直接返回，如果有异步执行中，不会重复触发异步，但共享异步结果。
+ *
+ * asyncMemo(()=>download({ fssid: 'a' }), 'a', { persisted: false }); // 不读取缓存结果，但是结果还是会缓存。
+ * asyncMemo(()=>download({ fssid: 'a' })); // 没有缓存键时，直接执行异步方法，不读取缓存结果，也不会缓存异步结果。
  */
 class AsyncMemo<DataType = any> {
   private promiseCache: Record<string, Promise<any>>;
