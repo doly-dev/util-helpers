799688ce2955f8688705ed02522839a3ecc6196e
caijf <369756941@qq.com>
2025-11-05 17:12:49 +0800

    test: update ipv6 链路本地地址

--- test/isIPv6.test.ts
+++ test/isIPv6.test.ts
@@ -8,6 +8,8 @@ describe('isIPv6', () => {
   });
 
   it('correct', () => {
+    // IPv6链路本地地址(一般网段是fe80::/10)是用来保证链路之间的通信（非路由通信），一般是根据MAC地址用EUI-64算法生成。
+    expect(isIPv6('2001:db8:130f:0:0:9c0:876a:1%0b')).toBe(true);
     expect(isIPv6('2001:0DB8:0000:0023:0008:0800:200C:417A')).toBe(true);
     expect(isIPv6('2001:DB8:0:23:8:800:200C:417A')).toBe(true);
     expect(isIPv6('FF01:0:0:0:0:0:0:1101')).toBe(true);
