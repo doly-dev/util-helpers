5b59239ca428a903f4e855877b440703ca688a91
caijf <369756941@qq.com>
2025-12-08 19:07:23 +0800

    chore: update dev deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['dist/', 'dist-bak/', 'types/', 'docs/', 'coverage/', 'lib/', 'esm/', 'es/']
   },
