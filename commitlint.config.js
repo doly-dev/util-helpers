6891b34925e04bed678361064873fadb91f45a4f
caijf <369756941@qq.com>
2026-09-07 11:56:00 +0800

    chore(commitlint): 更新配置文件，添加自定义规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// ref: https://commitlint.js.org/reference/configuration.html
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [0],
+    'body-max-line-length': [2, 'always', 300]
+  }
+};
